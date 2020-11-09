34f1d666453d3be823a4c897b8f40c2ba09f4dd5
e-operator <67810039+e-operator@users.noreply.github.com>
2020-11-09 18:00:28 -0500

    Fixed ghostbin downloading with webdriver cloudflare bypass
    Selenium webdriver chrome is now required

--- leak-dl.py
+++ leak-dl.py
@@ -6,7 +6,7 @@ from nitter_scraper import NitterScraper
 from colorama import init, Fore, Style
 import datetime
 from datetime import timedelta
-#from selenium import webdriver
+from selenium import webdriver
 import urllib.request
 import cfscrape
 import argparse
@@ -18,8 +18,9 @@ import re
 #init colorama
 init()
 
+#setup argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("--cooldown",help="Time in seconds to wait between downloads. Values <3 may result in download failure.",default=3,type=float)
+#parser.add_argument("--cooldown",help="Time in seconds to wait between downloads. Values <3 may result in download failure.",default=3,type=float)
 parser.add_argument("--no-remove-dupes",help="Do not remove duplicate files in ./pastes after all downloads are complete.",dest='noremovedupes',default=False,action='store_true')
 args = parser.parse_args()
 
@@ -32,15 +33,17 @@ print(r"| |    / _ \/ _` | |/ /______| | | | |    ")
 print(r"| |___|  __/ (_| |   <       | |/ /| |____")
 print(r"\_____/\___|\__,_|_|\_\      |___/ \_____/")
 
-print("Starting..")
+print("Starting...")
 
+#handle leftover log.log on incomplete shutdown (KeyboardInterrupt)
 try:
     os.remove("./log/log.log")
 except:
-    print("log.log not detected; skipping deletion...")
+    print()
 else:
     print("Deleting old download list...")
 
+#save tweets containing URLs to file ./log/log.log
 for user in users:
     for tweet in nitter_scraper.get_tweets(user, pages=100):
         print()
@@ -49,26 +52,50 @@ for user in users:
         f.write(tweet.dict()["text"] + '\n')
         f.close()
 
+#open that file
 s = open("./log/log.log", "r", encoding='utf8')
 
-firstsort = re.findall(r'pastebin.com\S+', s.read())
+#find ghostbin pastes and store in list
+ghostsort = re.findall(r'ghostbin.co\S+', s.read())
 
-for x in firstsort:
+#find pastebin pastes and store in list
+pastesort = re.findall(r'pastebin.com\S+', s.read())
+
+#close log.log
+s.close()
+
+#setup selenium for ghostbin because cloudflare >:(
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument("--disable-blink-features=AutomationControlled")
+browser = webdriver.Chrome(options=options)
+
+#to tell whether or not to wait for initial cloudflare warning
+run = 0
+
+#download all ghostbin pastes it can
+for x in ghostsort:
     now = datetime.datetime.now()
     file_savename = now.strftime("%m-%d-%y-%H-%M-%S")
     print(Style.BRIGHT + "[" + Fore.GREEN + "+" + Fore.RESET + "] " + Style.RESET_ALL + "Paste URL Found: " + Fore.GREEN + x + Fore.RESET)
     try:
-        urllib.request.urlretrieve("https://" + x, './pastes/' + file_savename + ".txt" )
+        browser.get('https://' + x)
+        if(run!=0):
+            content = browser.find_element_by_css_selector('pre')
+            f = open('./pastes/' + file_savename + ".txt", 'w', encoding='utf8')
+            f.write(content.get_attribute('innerHTML'))
+            f.close()
+        else:
+            time.sleep(30) # wait for cloudflare to piss off & give time to solve hcaptcha
     except:
         print(Style.BRIGHT + "[" + Fore.RED + "-" + Fore.RESET + "] " + Fore.RED + "Download error: Possible 404" + Fore.RESET + Style.RESET_ALL)
     else:
         print(Style.BRIGHT + "[" + Fore.GREEN + "+" + Fore.RESET + "] " + Fore.GREEN + "Downloaded!" + Fore.RESET + Style.RESET_ALL)
-
-    time.sleep(3)
+    run = run + 1
+    time.sleep(1)
 
 removed = 0
-
-s.close()
 
 if args.noremovedupes == False:
     def md5(f):
